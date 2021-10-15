e47b8295e2cfe60daef212a8efdef4590a71c649
Mino1289 <kretzvictor@orange.fr>
2021-10-15 23:06:03 +0200

    [apps/code] Remove tests for scripts

--- python/test/basics.cpp
+++ python/test/basics.cpp
@@ -8,8 +8,5 @@ QUIZ_CASE(python_basics) {
 }
 
 QUIZ_CASE(python_template) {
-  assert_script_execution_succeeds(Code::ScriptTemplate::Squares()->content());
-  assert_script_execution_succeeds(Code::ScriptTemplate::Mandelbrot()->content());
-  assert_script_execution_succeeds(Code::ScriptTemplate::Polynomial()->content());
-  assert_script_execution_succeeds(Code::ScriptTemplate::Parabola()->content());
+  assert_script_execution_succeeds(Code::ScriptTemplate::Mathsup()->content());
 }
