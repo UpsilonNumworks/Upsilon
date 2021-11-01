2c5aef4e17546796b48f66f418b912d4fa79ce5e
devdl11 <54149885+devdl11@users.noreply.github.com>
2021-11-01 16:34:09 +0100

    Update apps/usb/usb_view.cpp
    Co-authored-by: Lauryy06 <80424145+Lauryy06@users.noreply.github.com>

--- apps/usb/usb_view.cpp
+++ apps/usb/usb_view.cpp
@@ -1,13 +1,10 @@
 #include "usb_view.h"
 #include <assert.h>
 
-namespace USB
-{
-    USBView::USBView(I18n::Message *messages, KDColor *fgcolors, KDColor *bgcolors, uint8_t numberOfMessages)
-    {
+namespace USB {
+    USBView::USBView(I18n::Message *messages, KDColor *fgcolors, KDColor *bgcolors, uint8_t numberOfMessages) {
         m_numberOfMessages = numberOfMessages < k_maxNumberOfMessages ? numberOfMessages : k_maxNumberOfMessages;
-        for (uint8_t i = 0; i < m_numberOfMessages; i++)
-        {
+        for (uint8_t i = 0; i < m_numberOfMessages; i++) {
             m_messageTextViews[i].setFont(i == 0 ? KDFont::LargeFont : KDFont::SmallFont);
             m_messageTextViews[i].setMessage(messages[i]);
             m_messageTextViews[i].setAlignment(0.5f, 0.5f);
@@ -16,15 +13,12 @@ namespace USB
         }
     }
 
-    void USBView::drawRect(KDContext *ctx, KDRect rect) const
-    {
+    void USBView::drawRect(KDContext *ctx, KDRect rect) const {
         ctx->fillRect(bounds(), Palette::BackgroundHard);
     }
 
-    View *USBView::subviewAtIndex(int index)
-    {
-        if (index >= m_numberOfMessages)
-        {
+    View *USBView::subviewAtIndex(int index) {
+        if (index >= m_numberOfMessages) {
             assert(false);
             return nullptr;
         }
@@ -33,8 +27,7 @@ namespace USB
 
     void USBView::update(I18n::Message *messages, KDColor *fgcolors, KDColor *bgcolors, uint8_t numberOfMessages){
         m_numberOfMessages = numberOfMessages < k_maxNumberOfMessages ? numberOfMessages : k_maxNumberOfMessages;
-        for (uint8_t i = 0; i < m_numberOfMessages; i++)
-        {
+        for (uint8_t i = 0; i < m_numberOfMessages; i++) {
             m_messageTextViews[i].setFont(i == 0 ? KDFont::LargeFont : KDFont::SmallFont);
             m_messageTextViews[i].setMessage(messages[i]);
             m_messageTextViews[i].setAlignment(0.5f, 0.5f);
@@ -44,22 +37,19 @@ namespace USB
         reload();
     }
 
-    void USBView::reload(){
+    void USBView::reload() {
         markRectAsDirty(bounds());
     }    
 
-    void USBView::layoutSubviews(bool force)
-    {
-        if (m_numberOfMessages == 0)
-        {
+    void USBView::layoutSubviews(bool force) {
+        if (m_numberOfMessages == 0) {
             return;
         }
         KDCoordinate width = bounds().width();
         KDCoordinate titleHeight = m_messageTextViews[0].minimalSizeForOptimalDisplay().height();
         KDCoordinate textHeight = KDFont::SmallFont->glyphSize().height();
         m_messageTextViews[0].setFrame(KDRect(0, k_titleMargin, width, titleHeight), force);
-        for (uint8_t i = 1; i < m_numberOfMessages; i++)
-        {
+        for (uint8_t i = 1; i < m_numberOfMessages; i++) {
             m_messageTextViews[i].setFrame(KDRect(0, k_paragraphHeight + (i - 1) * textHeight, width, textHeight), force);
         }
     }
