3331b13b46646eef5e15ab8664bbec6e8b58f37f
Yaya.Cout <yaya.cout@free.fr>
2021-11-28 18:05:59 +0100

    [apps/settings] Fix voltage in settings

--- apps/settings/sub_menu/about_controller.cpp
+++ apps/settings/sub_menu/about_controller.cpp
@@ -102,13 +102,13 @@ bool AboutController::handleEvent(Ion::Events::Event event) {
       }
       if(childLabel == I18n::Message::Battery){
         MessageTableCellWithBuffer * myCell = (MessageTableCellWithBuffer *)m_selectableTableView.selectedCell();
-        char batteryLevel[15];
+        char batteryLevel[6];
         if(strchr(myCell->accessoryText(), '%') == NULL){
-          int batteryLen = Poincare::Integer((int) ((Ion::Battery::voltage() - 3.6) * 166)).serialize(batteryLevel, 15);
+          int batteryLen = Poincare::Integer((int) ((Ion::Battery::voltage() - 3.6) * 166)).serialize(batteryLevel, 6);
           batteryLevel[batteryLen] = '%';
           batteryLevel[batteryLen+1] = '\0';
         }else{
-          int batteryLen = Poincare::Number::FloatNumber(Ion::Battery::voltage()).serialize(batteryLevel, 15, Poincare::Preferences::PrintFloatMode::Decimal, 3);
+          int batteryLen = Poincare::Number::FloatNumber(Ion::Battery::voltage()).serialize(batteryLevel, 6, Poincare::Preferences::PrintFloatMode::Decimal, 3);
           batteryLevel[batteryLen] = 'V';
           batteryLevel[batteryLen+1] = '\0';
         }
@@ -181,8 +181,8 @@ void AboutController::willDisplayCellForIndex(HighlightCell * cell, int index) {
     MessageTableCellWithBuffer * myCell = (MessageTableCellWithBuffer *)cell;
     static const char * mpVersion = MICROPY_VERSION_STRING;
 
-    static char batteryLevel[5];
-    int batteryLen = Poincare::Number::FloatNumber(Ion::Battery::voltage()).serialize(batteryLevel, 4, Poincare::Preferences::PrintFloatMode::Decimal, 3);
+    static char batteryLevel[6];
+    int batteryLen = Poincare::Number::FloatNumber(Ion::Battery::voltage()).serialize(batteryLevel, 6, Poincare::Preferences::PrintFloatMode::Decimal, 3);
     batteryLevel[batteryLen] = 'V';
     batteryLevel[batteryLen + 1] = '\0';
 
