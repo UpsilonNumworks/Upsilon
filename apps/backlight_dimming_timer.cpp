35ef1b519ffdd7dea67c5ac457c6709d732f8a81
Yaya.Cout <yaya.cout@free.fr>
2021-12-18 13:16:10 +0100

    Add natural dimer

--- apps/backlight_dimming_timer.cpp
+++ apps/backlight_dimming_timer.cpp
@@ -1,4 +1,11 @@
 #include "backlight_dimming_timer.h"
+#include "global_preferences.h"
+
+int nbrofrun = 0;
+int brightnessLevel;
+int nbrofsteps;
+int nextbrightness;
+float timeToSleep; // In miliseconds
 
 BacklightDimmingTimer::BacklightDimmingTimer() :
   Timer(k_idleBeforeDimmingDuration/Timer::TickDuration)
@@ -6,6 +13,33 @@ BacklightDimmingTimer::BacklightDimmingTimer() :
 }
 
 bool BacklightDimmingTimer::fire() {
-  Ion::Backlight::setBrightness(k_dimBacklightBrightness);
+  if (nbrofrun == 0) {
+    brightnessLevel = GlobalPreferences::sharedGlobalPreferences()->brightnessLevel();
+    nbrofsteps = brightnessLevel / decreaseby;
+    timeToSleep = decreasetime / nbrofsteps;
+    m_period = timeToSleep / Timer::TickDuration;
+    if (m_period == 0) {
+      m_period = 1;
+    }
+    resetTimer();
+  }
+  if (nbrofrun < nbrofsteps) {
+    nextbrightness = (brightnessLevel-k_dimBacklightBrightness)/nbrofsteps * (nbrofsteps-nbrofrun);
+    Ion::Backlight::setBrightness(nextbrightness);
+    resetTimer();
+  } else if (nbrofrun == nbrofsteps) {
+    Ion::Backlight::setBrightness(k_dimBacklightBrightness);
+  }
+  nbrofrun++;
   return false;
+}
+
+void BacklightDimmingTimer::reset() {
+  nbrofrun = 0;
+  m_period = k_idleBeforeDimmingDuration / Timer::TickDuration;
+  resetTimer();
+}
+
+void BacklightDimmingTimer::resetTimer() {
+  BacklightDimmingTimer::m_numberOfTicksBeforeFire = BacklightDimmingTimer::m_period;
 }
--- apps/backlight_dimming_timer.h
+++ apps/backlight_dimming_timer.h
@@ -6,10 +6,14 @@
 class BacklightDimmingTimer : public Timer {
 public:
   BacklightDimmingTimer();
+  void reset();
 private:
   constexpr static int k_idleBeforeDimmingDuration = 30*1000; // In miliseconds
   constexpr static int k_dimBacklightBrightness = 0;
+  constexpr static int decreaseby = 15;
+  constexpr static int decreasetime = 1*1000; // In miliseconds
   bool fire() override;
+  void resetTimer();
 };
 
 #endif
