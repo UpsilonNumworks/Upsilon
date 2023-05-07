9f8e8d024d457e20a0a63c2ef29d53065ccf53a2
Heath123 <heath.mitchell27@gmail.com>
2023-05-07 12:54:53 +0100

    Add missing file

---
+++ ion/src/simulator/prizm/power.cpp
@@ -0,0 +1,39 @@
+#include <ion/power.h>
+#include <stdint.h>
+#include <string.h>
+
+#include "main.h"
+
+#include <gint/gint.h>
+#include <gint/display.h>
+
+void PowerOff(int displayLogo) {
+  __asm__ __volatile__ (
+    "mov.l 2f, r2 \n\t"
+    "mov.l 1f, r0 \n\t"
+    "jmp @r2 \n\t"
+    "nop \n\t"
+    "1: \n\t"
+    ".long 0x1839 \n\t"
+    "2: \n\t"
+    ".long 0x80020070 \n\t"
+  );
+}
+
+void powerOff(void) {
+  PowerOff(1);
+}
+
+namespace Ion {
+namespace Power {
+
+void suspend(bool checkIfOnOffKeyReleased) {
+  Simulator::Main::runPowerOffSafe(powerOff, true);
+}
+
+void standby() {
+  Simulator::Main::runPowerOffSafe(powerOff, true);
+}
+
+}
+}
