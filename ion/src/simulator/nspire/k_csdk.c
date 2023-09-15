be464cc278af602f6dd94be10550b9c8ea968842
Luminoso-256 <63971285+Luminoso-256@users.noreply.github.com>
2023-09-14 20:34:37 -0500

    Fix the nspire port (ergo: framebuffer functions, shift/ctrl don't corrupt things due to weird draw calls)

--- ion/src/simulator/nspire/k_csdk.c
+++ ion/src/simulator/nspire/k_csdk.c
@@ -584,10 +584,10 @@ void statusflags(){
     else
       msg="";
   }
-  os_fill_rect(0,0,LCD_WIDTH_PX,16,SDK_BLACK);
-  os_draw_string_medium(225,-STATUS_AREA_PX,statuscolor,SDK_BLACK,msg,false);
-  os_draw_string_medium(160,-STATUS_AREA_PX,statuscolor,SDK_BLACK,os_get_angle_unit()?" rad ":" deg ",false);  
-  display_time();
+  // os_fill_rect(0,0,LCD_WIDTH_PX,16,SDK_BLACK);
+  // os_draw_string_medium(225,-STATUS_AREA_PX,statuscolor,SDK_BLACK,msg,false);
+  // os_draw_string_medium(160,-STATUS_AREA_PX,statuscolor,SDK_BLACK,os_get_angle_unit()?" rad ":" deg ",false);  
+  // display_time();
 }
 void statusline(int mode){
   statusflags();
@@ -617,7 +617,7 @@ void statusline(int mode){
 #include "k_defs.h"
 
 void sdk_init(void){
-  lcd_init(lcd_type()); // clrscr();
+  lcd_init(lcd_type());
 }
 
 void sdk_end(void){
@@ -993,6 +993,7 @@ int nspire_shift=0;
 int nspire_ctrl=0;
 int nspire_select=false;
 void statusline(int mode){
+  return; //DON'T DO ANYTHING YOU IDIOT
   char *msg=0;
   if (nspire_ctrl){
     if (nspire_shift)
