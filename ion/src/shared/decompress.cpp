81675bc15f514997d0d18402101655780b501816
Leviathan3DPrinting <leviathan3dprinting@gmail.com>
2024-05-10 16:31:32 -0500

    revert file

--- ion/src/shared/decompress.cpp
+++ ion/src/shared/decompress.cpp
@@ -4,10 +4,5 @@
 void Ion::decompress(const uint8_t * src, uint8_t * dst, int srcSize, int dstSize) {
   int outputSize = LZ4_decompress_safe(reinterpret_cast<const char *>(src), reinterpret_cast<char *>(dst), srcSize, dstSize);
   (void)outputSize; // Make the compiler happy if assertions are disabled
-  if (outputSize < 0 || outputSize <= dstSize) { // Check the data came back valid
-    outputSize = LZ4_decompress_safe(reinterpret_cast<const char *>(src), reinterpret_cast<char *>(dst), srcSize, dstSize);
-  }
-  else {
-    assert(outputSize == dstSize);
-  }
+  assert(outputSize == dstSize);
 }
